f62aa5f2b14ef897e491d99058f3f1ebf656af62
kevinMG01 <montanokevin46@gmail.com>
2024-04-25 13:56:57 -0300

    creacion de la funcion "generate_block"
    esta función instancia la escena “BlockRed” y “BlockViolet”

--- ejercicio_senales/scenes/Buttons/RedGenerator/RedGenerator.gd
+++ ejercicio_senales/scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,16 +1,14 @@
 extends TextureButton
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var bloque_rojo = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 
 func _on_button_down():
-	
+	generate_block()
 	pass # Replace with function body.
+
+
+func generate_block():
+	var instanciar_bloquerojo = bloque_rojo.instantiate()
+	add_child(instanciar_bloquerojo)
+	pass
--- ejercicio_senales/scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ ejercicio_senales/scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,15 +1,14 @@
 extends TextureButton
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+var bloque_violeta = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
 func _on_button_down():
+	generate_block()
 	pass # Replace with function body.
+
+
+
+func generate_block():
+	var instanciar_bloquevioleta = bloque_violeta.instantiate()
+	add_child(instanciar_bloquevioleta)
+	pass
